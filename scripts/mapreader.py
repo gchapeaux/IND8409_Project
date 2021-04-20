53a08dff61f64ee49121a84088575a8d85b9650b
Grégoire Chapeaux <gchapeaux@gmail.com>
2021-04-20 18:14:08 -0400

    mapreader

--- scripts/mapreader.py
+++ scripts/mapreader.py
@@ -1,28 +1,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.colors import LinearSegmentedColormap
+from matplotlib.colors import ListedColormap, BoundaryNorm
 
-map_colors = {
-    '@' : (0,0,0),
-    '.' : (150,150,150),
-    'T' : (0,255,0),
-    'W' : (0,0,255),
-    'S' : (50,50,255)
-}
-plt.register_cmap(cmap=LinearSegmentedColormap('MapColors', map_colors))
+ATLAS_INDEXES = {'@' : 0, '.' : 1, 'T' : 2, 'W' : 3, 'S' : 4}
+indexing = np.vectorize(lambda s : ATLAS_INDEXES[s])
+
+color_list = [(0.0,0.0,0.0), (0.5,0.5,0.5), (0.0,0.3,0.0), (0.0,0.0,1.0),(0.2,0.2,1.0)]
+ATLAS_COLORS = ListedColormap(color_list, name='colors', N=256)
+ATLAS_NORM = BoundaryNorm([0,1,2,3,4], 4)
 
 def read_map(path_to_map):
     map_mx = []
     with open(path_to_map) as mapfile:
         lines = mapfile.readlines()
     for line in lines[4:517]:
-        map_line = line.split()
+        map_line = list(line.replace('\n',''))
         map_mx.append(map_line)
-    return map_mx
+    return np.array(map_mx)
 
-def viz_map(map_mx, colors=map_colors):
-    plt.imshow(map_mx, cmap=plt.get_cmap('MapColors'))
+def viz_map(map_mx):
+    plt.imshow(indexing(map_mx), cmap=ATLAS_COLORS, norm=ATLAS_NORM)
     plt.show()
-
-test = read_map('data/losttemple.map')
-viz_map(test)
