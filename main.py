8b3550033e85a4892cd1422d384100ba0febcb4c
Grégoire Chapeaux <gchapeaux@gmail.com>
2021-04-21 22:24:15 -0400

    map

--- main.py
+++ main.py
@@ -24,10 +24,11 @@ def main(HEADLESS=True, SAVING=True):
     escape = False
     i=0
 
-    world = World('data/battleground.map', n_robots=50, spawn_radius=50)
+    world = World('data/battleground.map', n_robots=50, spawn_radius=20)
 
     print("| World generated, entering simulation")
-    
+    map_size = world.worldMap.shape[0]*world.worldMap.shape[1]
+
     fig, axes = plt.subplots(2,3)
     if not(HEADLESS):
         plt.ion()
@@ -41,16 +42,25 @@ def main(HEADLESS=True, SAVING=True):
         except:
             escape = False
         if (i%10 == 0):
-            print('| Simulation running, press ESCAPE to end |{}|'.format(run_dict[(i//10)%8]), end='\r')
+            print('| Simulation running, press ESCAPE to end - Step {} |{}|'.format(i, run_dict[(i//10)%8]), end='\r')
         world.step(fig, axes, HEADLESS, i)
+
+        for robot in world.robots.values():
+            if robot.exploration_history[-1]/map_size > 0.95:
+                escape = True
+
         i += 1
+    
+    print()
+
+    if not(HEADLESS):
+        plt.ioff()
 
     if SAVING:
         plt.close()
         
         viz_map(world.worldMap, "Global world map", os.path.join(output_path, "world.png"))
         
-        map_size = world.worldMap.shape[0]*world.worldMap.shape[1]
         completion = {}
         
         for robot in world.robots.values():
@@ -58,17 +68,17 @@ def main(HEADLESS=True, SAVING=True):
             save_path = os.path.join(output_path, "map_robot_{}_.png".format(robot.id))
             fig_name = "Map of the robot {} after {} steps".format(robot.id, i)
             viz_map(robot.dynamicMap.map, fig_name, save_path)
-
             completion[robot.id] = np.array(robot.exploration_history)/map_size
 
         for rid, hist in completion.items():
             np.random.seed(rid)
-            color = list(np.random.choice(range(256), size=3))
+            color = np.random.rand(3)/2.0
             plt.plot(hist, color=color)
+        plt.legend(labels=completion.keys())
+        plt.suptitle('Completion of the maps over time')
+        plt.savefig(os.path.join(output_path, 'completion.png'))
+        plt.show()
 
-
-    if not(HEADLESS):
-        plt.ioff()
     print("| Simulation ended. Check robot maps in output folder.")
 #EndRegion
 
